e98359c4e372fde9506b60b47fc5c96f49a59981
Kevin Maier <kevin.maier@uni-ulm.de>
2023-03-31 10:35:19 +0200

    Added the RavenDB binding

---
+++ ravendb/README.md
@@ -0,0 +1,42 @@
+<!--
+Copyright (c) 2012 - 2016 YCSB contributors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License"); you
+may not use this file except in compliance with the License. You
+may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+implied. See the License for the specific language governing
+permissions and limitations under the License. See accompanying
+LICENSE file.
+-->
+
+## Quick Start
+
+This section describes how to run YCSB on RavenDB.
+
+###  RavenDB Configuration Parameters
+
+* `ravendb.url` (required): defines the RavenDB endpoint, format `http://IP`, the binding uses the default port `8080`
+
+* `port` (optional, default=8080):  the port that the RavenDB database listens on
+
+* `ravendb.dbname` (required):  the name of the RavenDB database to use for the YCSB, e.g. `ycsb`
+
+* `batchsize` (optional, default=1):  batch insert size, e.g. `1000`
+
+* `debug` (optional, default=false):  print debug messages
+
+### Run YCSB
+
+Now you are ready to run! First, use the asynchronous driver to load the data:
+
+    ./bin/ycsb load ravendb -s -P workloads/workloada > outputLoad.txt
+
+Then, run the workload:
+
+    ./bin/ycsb run ravendb -s -P workloads/workloada > outputRun.txt
---
+++ ravendb/src/main/java/site/ycsb/db/RavenDbClient.java
@@ -0,0 +1,327 @@
+package site.ycsb.db;
+
+import net.ravendb.client.documents.DocumentStore;
+import net.ravendb.client.serverwide.DatabaseRecord;
+import net.ravendb.client.serverwide.operations.CreateDatabaseOperation;
+import net.ravendb.client.serverwide.operations.GetDatabaseNamesOperation;
+import okhttp3.*;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import site.ycsb.*;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+/**
+ * RavenDB binding for YCSB framework.
+ */
+public class RavenDbClient extends DB {
+  private static final OkHttpClient CLIENT = new OkHttpClient().newBuilder().build();
+
+  private static String url;
+  public static final String PORT_PROPERTY = "port";
+  public static final String PORT_PROPERTY_DEFAULT = "8080";
+  public static final String DBNAME_PROPERTY = "ravendb.dbname";
+  public static final String DBNAME_PROPERTY_DEFAULT = "ycsb";
+  private static String databaseName;
+  private final List<String> batchInserts = new ArrayList<>();
+  private static boolean debug = false;
+
+  /**
+   * The batch size to use for inserts.
+   */
+  private static int batchSize;
+
+  private static final AtomicBoolean DB_INIT_COMPLETE = new AtomicBoolean();
+  private static final MediaType MEDIA_TYPE = MediaType.parse("application/json");
+
+  /**
+   * Delete a record from the database.
+   *
+   * @param table The name of the table
+   * @param key   The record key of the record to delete.
+   * @return Zero on success, a non-zero error code on error. See the {@link DB}
+   * class's description for a discussion of error codes.
+   */
+  @Override
+  public Status delete(String table, String key) {
+    String path = "/docs?id=" + key;
+    Request request = requestBuilder(null, "DELETE", path);
+    try (Response response = CLIENT.newCall(request).execute()) {
+      if (response.isSuccessful()) {
+        return Status.OK;
+      } else {
+        System.err.println(Objects.requireNonNull(response.body()).string());
+        return Status.ERROR;
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+      return Status.ERROR;
+    }
+  }
+
+  /**
+   * Initialize any state for this DB. Called once per DB instance; there is one
+   * DB instance per client thread.
+   */
+  @Override
+  public void init() {
+    synchronized (DB_INIT_COMPLETE) {
+      if (!DB_INIT_COMPLETE.get()) {
+        Properties props = getProperties();
+        // Set insert batchsize, default 1 - to be YCSB-original equivalent
+        batchSize = Integer.parseInt(props.getProperty("batchsize", "1"));
+        debug = Boolean.parseBoolean(props.getProperty("debug", "false"));
+        databaseName = props.getProperty(DBNAME_PROPERTY, DBNAME_PROPERTY_DEFAULT);
+        url = props.getProperty("ravendb.url", null);
+        String port = props.getProperty(PORT_PROPERTY, PORT_PROPERTY_DEFAULT);
+        if (url == null) {
+          url = "http://localhost:" + port;
+        } else {
+          url += ":" + port;
+        }
+        DocumentStore store =
+            new DocumentStore("http://localhost:8080", "Northwind");
+        store.initialize();
+        GetDatabaseNamesOperation operation = new GetDatabaseNamesOperation(0, 25);
+        String[] databaseNames = store.maintenance().server().send(operation);
+        List<String> list = Arrays.asList(databaseNames);
+        if (!list.contains(databaseName)) {
+          DatabaseRecord databaseRecord = new DatabaseRecord();
+          databaseRecord.setDatabaseName(databaseName);
+          store.maintenance().server().send(new CreateDatabaseOperation(databaseRecord));
+        }
+        store.close();
+        DB_INIT_COMPLETE.set(true);
+        System.out.println("Setup completed with URL: " + url);
+      }
+    }
+  }
+
+  /**
+   * Insert a record in the database. Any field/value pairs in the specified
+   * values HashMap will be written into the record with the specified record
+   * key.
+   *
+   * @param table  The name of the table
+   * @param key    The record key of the record to insert.
+   * @param values A HashMap of field/value pairs to insert in the record
+   * @return Zero on success, a non-zero error code on error. See the {@link DB}
+   * class's description for a discussion of error codes.
+   */
+  @Override
+  public Status insert(String table, String key,
+                       Map<String, ByteIterator> values) {
+    String httpMethod = "PUT";
+    String path = "/docs?id=" + key;
+    String records = "";
+    if (batchSize == 1) {
+      records = insertBuilder(values);
+    } else {
+      httpMethod = "POST";
+      path = "/bulk_docs";
+      batchInserts.add(batchedInsertBuilder(values, key));
+      if (batchInserts.size() == batchSize) {
+        records += "{\n\"Commands\": [";
+        StringJoiner batchRecord = new StringJoiner(",\n");
+        for (String record : batchInserts) {
+          batchRecord.add(record);
+        }
+        records += batchRecord + "\n]\n}";
+        batchInserts.clear();
+      } else {
+        return Status.BATCHED_OK;
+      }
+    }
+
+    RequestBody insertBody = RequestBody.create(records, MEDIA_TYPE);
+    Request request = requestBuilder(insertBody, httpMethod, path);
+    System.out.println(request.headers());
+    try (Response response = CLIENT.newCall(request).execute()) {
+      if (response.isSuccessful()) {
+        return Status.OK;
+      } else {
+        System.err.println(Objects.requireNonNull(response.body()).string());
+        return Status.ERROR;
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+      return Status.ERROR;
+    }
+  }
+
+  /**
+   * Read a record from the database. Each field/value pair from the result will
+   * be stored in a HashMap.
+   *
+   * @param table  The name of the table
+   * @param key    The record key of the record to read.
+   * @param fields The list of fields to read, or null for all of them
+   * @param result A HashMap of field/value pairs for the result
+   * @return Zero on success, a non-zero error code on error or "not found".
+   */
+  @Override
+  public Status read(String table, String key, Set<String> fields,
+                     Map<String, ByteIterator> result) {
+    String path = "/docs?id=" + key;
+    Request request = requestBuilder(null, "GET", path);
+    try (Response response = CLIENT.newCall(request).execute()) {
+      if (response.isSuccessful()) {
+        if (debug) {
+          JSONObject json = new JSONObject(Objects.requireNonNull(response.body()).string());
+          JSONArray jsonArray = json.getJSONArray("Results");
+          fillMap(result, jsonArray);
+        }
+        return Status.OK;
+      } else {
+        System.err.println(Objects.requireNonNull(response.body()).string());
+        return Status.ERROR;
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+      return Status.ERROR;
+    }
+  }
+
+  /**
+   * Perform a range scan for a set of records in the database. Each field/value
+   * pair from the result will be stored in a HashMap.
+   *
+   * @param table       The name of the table
+   * @param startkey    The record key of the first record to read.
+   * @param recordcount The number of records to read
+   * @param fields      The list of fields to read, or null for all of them
+   * @param result      A Vector of HashMaps, where each HashMap is a set field/value
+   *                    pairs for one record
+   * @return Zero on success, a non-zero error code on error. See the {@link DB}
+   * class's description for a discussion of error codes.
+   */
+  @Override
+  public Status scan(String table, String startkey, int recordcount,
+                     Set<String> fields, Vector<HashMap<String, ByteIterator>> result) {
+    StringBuilder path = new StringBuilder("/docs?id=" + startkey);
+    String regex = "[a-zA-Z]+([0-9]+)";
+    Matcher matcher = Pattern.compile(regex).matcher(startkey);
+    int numKey = 0;
+    if (matcher.find()) {
+      numKey = Integer.parseInt(matcher.group(1));
+    }
+    for (int i = 0; i < recordcount; i++) {
+      path.append("&id=user").append(numKey + i);
+    }
+    Request request = requestBuilder(null, "GET", path.toString());
+    try (Response response = CLIENT.newCall(request).execute()) {
+      if (response.isSuccessful()) {
+        if (debug) {
+          JSONObject json = new JSONObject(Objects.requireNonNull(response.body()).string());
+          JSONArray jsonArray = json.getJSONArray("Results");
+          for (int i = 0; i < jsonArray.length(); i++) {
+            HashMap<String, ByteIterator> resultMap = new HashMap<String, ByteIterator>();
+            fillMap(resultMap, jsonArray);
+            result.add(resultMap);
+          }
+        }
+        return Status.OK;
+      } else {
+        System.err.println(Objects.requireNonNull(response.body()).string());
+        return Status.ERROR;
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+      return Status.ERROR;
+    }
+  }
+
+  /**
+   * Update a record in the database. Any field/value pairs in the specified
+   * values HashMap will be written into the record with the specified record
+   * key, overwriting any existing values with the same field name.
+   *
+   * @param table  The name of the table
+   * @param key    The record key of the record to write.
+   * @param values A HashMap of field/value pairs to update in the record
+   * @return Zero on success, a non-zero error code on error. See this class's
+   * description for a discussion of error codes.
+   */
+  @Override
+  public Status update(String table, String key,
+                       Map<String, ByteIterator> values) {
+    String httpMethod = "PUT";
+    String path = "/docs?id=" + key;
+    String records;
+    records = insertBuilder(values);
+
+    RequestBody insertBody = RequestBody.create(records, MEDIA_TYPE);
+    Request request = requestBuilder(insertBody, httpMethod, path);
+    try (Response response = CLIENT.newCall(request).execute()) {
+      if (response.isSuccessful()) {
+        return Status.OK;
+      } else {
+        System.err.println(Objects.requireNonNull(response.body()).string());
+        return Status.ERROR;
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+      return Status.ERROR;
+    }
+  }
+
+  private String insertBuilder(Map<String, ByteIterator> values) {
+    StringBuilder records = new StringBuilder(" {\n");
+
+    for (Map.Entry<String, ByteIterator> entry : values.entrySet()) {
+      String value = entry.getValue().toString();
+      // Escape backslash and quotation marks to get valid json
+      value = value.replaceAll("\\\\", "\\\\\\\\");
+      value = value.replaceAll("\"", "\\\\\"");
+      records.append("    \"").append(entry.getKey()).append("\": \"").append(value).append("\",\n");
+    }
+    records.append("   \"@metadata\":{\n");
+    records.append("   \"@collection\":\"records\"\n");
+    records.append("   }\n");
+    records.append("}");
+    return records.toString();
+  }
+
+  private String batchedInsertBuilder(Map<String, ByteIterator> values, String key) {
+    return " {\n" + "   \"Id\":\"" + key + "\",\n" +
+        "   \"ChangeVector\": null,\n" +
+        "   \"Document\":" +
+        insertBuilder(values) +
+        "   \"Type\": \"PUT\"\n" +
+        "}";
+  }
+
+  //TODO implement authorization
+  private Request requestBuilder(RequestBody body, String method, String path) {
+    return new Request.Builder()
+        .url(url + "/databases/" + databaseName + path)
+        .method(method, body)
+        .addHeader("Content-Type", "application/json")
+        .addHeader("--key", "Test")
+        .addHeader("--cert", "TestValue")
+        .build();
+  }
+
+  protected void fillMap(Map<String, ByteIterator> resultMap, JSONArray jsonArray) {
+    for (Object o : jsonArray) {
+      if (!JSONObject.NULL.equals(o)) {
+        JSONObject jsonLineItem = (JSONObject) o;
+        for (String key : jsonLineItem.keySet()) {
+          if (key.equals("id") || key.contains("field")) {
+            resultMap.put(key, new StringByteIterator(jsonLineItem.get(key).toString()));
+          }
+        }
+      }
+    }
+  }
+}
